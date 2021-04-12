3718abdbc05357ccf856301e58936003bac0aa53
patmwells <patmicwells@gmail.com>
2021-04-12 13:19:04 -0700

    fix: tests

--- app/test/automation/cypress/integration/spec.js
+++ app/test/automation/cypress/integration/spec.js
@@ -3,7 +3,7 @@ describe('Automation End-To-End Tests', () => {
     it('should load the app successfully', () => {
         cy.visit('/');
 
-        cy.get('div').should('contain', 'Hello World!');
+        cy.get('div').should('contain', 'Hello IntroCard!');
     });
 
 });
