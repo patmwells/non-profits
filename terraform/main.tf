9deb422ae701d0aee2f6db8002c04c6424f5b3d8
patmwells <patmicwells@gmail.com>
2021-04-22 10:33:06 -0700

    feat: add deployment with terraform

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+
+  backend "local" {}
+
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
+variable "heroku_api_key" {
+  type = string
+  description = "The api key for the heroku account"
+  sensitive = true
+}
+
+variable "heroku_email" {
+  type = string
+  description = "The email for the heroku account"
+  sensitive = true
+}
+
+provider "heroku" {
+  email   = var.heroku_email
+  api_key = var.heroku_api_key
+}
+
+variable "node_env" {
+  description = "The NODE_ENV for the application"
+}
+
+resource "heroku_config" "common" {
+  vars = {
+    NODE_ENV = var.node_env
+  }
+}
+
+variable "app_name" {
+  description = "Name of the provisioned Heroku app"
+  default = "non-profits"
+}
+
+resource "heroku_app" "non-profits" {
+  name   = var.app_name
+  region = "us"
+  stack = "container"
+}
+
+resource "heroku_app_config_association" "non-profits" {
+  app_id = heroku_app.non-profits.id
+
+  vars = heroku_config.common.vars
+}
+
+variable "app_directory" {
+  description = "The directory for the app source code"
+}
+
+resource "heroku_build" "non-profits" {
+  app = heroku_app.non-profits.id
+
+  source {
+    path = var.app_directory
+  }
+}
+
+resource "heroku_formation" "non-profits" {
+  app = heroku_app.non-profits.id
+  type= "web"
+  quantity = 1
+  size = "Free"
+  depends_on = [heroku_build.non-profits]
+}
